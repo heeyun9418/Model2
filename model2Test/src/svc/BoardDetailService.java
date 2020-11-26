88910c5d315d013814886ef60edbf43eff3493e8
heeyun9418 <jjhhyy9418@gmail.com>
2020-11-26 09:51:05 +0900

    Model2_Test

---
+++ model2Test/src/svc/BoardDetailService.java
@@ -0,0 +1,27 @@
+package svc;
+
+import static db.JdbcUtil.*;
+import java.sql.Connection;
+
+import dao.BoardDAO;
+import vo.BoardBean;
+
+public class BoardDetailService {
+	public BoardBean getArticle(int board_num) throws Exception {
+		BoardBean article = null;
+		Connection conn = getConnection();
+		BoardDAO boardDAO = BoardDAO.getInstance();
+		boardDAO.setConnection(conn);
+		int updateCount = boardDAO.updateReadcount(board_num);
+
+		if (updateCount > 0) {
+			commit(conn);
+		} else {
+			rollback(conn);
+		}
+
+		article = boardDAO.selectArticle(board_num);
+		close(conn);
+		return article;
+	}
+}
